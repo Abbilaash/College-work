a9954a68a488b13daccc144d02aba5e5ef77c08e
Abbilaash A T <78356376+Abbilaash@users.noreply.github.com>
2024-10-06 02:22:32 +0530

    Update reverse.c

--- SEM3/DS lab/Linked list/reverse.c
+++ SEM3/DS lab/Linked list/reverse.c
@@ -1,70 +1,72 @@
-// Iterative C program to reverse a linked list
+#include<stdio.h>
+#include<stdlib.h>
 
-#include <stdio.h>
-
-struct Node {
-    int data;
-    struct Node* next;
+/* Link list node */
+struct Node
+{
+	int data;
+	struct Node* next;
 };
 
-// Given the head of a list, reverse the list and return the
-// head of reversed list
-struct Node* reverseList(struct Node* head) {
-
-    // Initialize three pointers: curr, prev and next
-    struct Node *curr = head, *prev = NULL, *next;
-
-    // Traverse all the nodes of Linked List
-    while (curr != NULL) {
-
-        // Store next
-        next = curr->next;
-
-        // Reverse current node's next pointer
-        curr->next = prev;
-
-        // Move pointers one position ahead
-        prev = curr;
-        curr = next;
-    }
-
-    // Return the head of reversed linked list
-    return prev;
+/* Function to reverse the linked list */
+static void reverse(struct Node** head_ref)
+{
+	struct Node* prev = NULL;
+	struct Node* current = *head_ref;
+	struct Node* next;
+	while (current != NULL)
+	{
+		next = current->next; 
+		current->next = prev; 
+		prev = current;
+		current = next;
+	}
+	*head_ref = prev;
 }
 
-void printList(struct Node* node) {
-    while (node != NULL) {
-        printf(" %d", node->data);
-        node = node->next;
-    }
+/* Function to push a node */
+void push(struct Node** head_ref, int new_data)
+{
+	/* allocate node */
+	struct Node* new_node =
+			(struct Node*) malloc(sizeof(struct Node));
+			
+	/* put in the data */
+	new_node->data = new_data;
+				
+	/* link the old list of the new node */
+	new_node->next = (*head_ref); 
+		
+	/* move the head to point to the new node */
+	(*head_ref) = new_node;
 }
 
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while(temp != NULL)
+	{
+		printf("%d ", temp->data); 
+		temp = temp->next; 
+	}
+} 
 
-struct Node* createNode(int new_data) {
-    struct Node* new_node
-        = (struct Node*)malloc(sizeof(struct Node));
-    new_node->data = new_data;
-    new_node->next = NULL;
-    return new_node;
-}
+/* Driver program to test above function*/
+int main()
+{
+	/* Start with the empty list */
+	struct Node* head = NULL;
 
-int main() {
-
-    // Create a hard-coded linked list:
-    // 1 -> 2 -> 3 -> 4 -> 5
-    struct Node* head = createNode(1);
-    head->next = createNode(2);
-    head->next->next = createNode(3);
-    head->next->next->next = createNode(4);
-    head->next->next->next->next = createNode(5);
-
-    printf("Given Linked list:");
-    printList(head);
-
-    head = reverseList(head);
-
-    printf("\nReversed Linked List:");
-    printList(head);
-
-    return 0;
+	push(&head, 20);
+	push(&head, 4);
+	push(&head, 15); 
+	push(&head, 85);	 
+	
+	printf("Given linked list\n");
+	printList(head); 
+	reverse(&head);					 
+	printf("\nReversed Linked list \n");
+	printList(head); 
+	getchar();
 }
