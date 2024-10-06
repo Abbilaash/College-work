c7233dd7e509513c7611392dd37725e16b976953
Abbilaash A T <78356376+Abbilaash@users.noreply.github.com>
2024-10-06 17:32:12 +0530

    Update reverse.c

--- SEM3/DS lab/Linked list/reverse.c
+++ SEM3/DS lab/Linked list/reverse.c
@@ -1,72 +1,57 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-/* Link list node */
-struct Node
-{
-	int data;
-	struct Node* next;
+struct Node {
+    int data;
+    struct Node* next;
 };
 
-/* Function to reverse the linked list */
-static void reverse(struct Node** head_ref)
-{
-	struct Node* prev = NULL;
-	struct Node* current = *head_ref;
-	struct Node* next;
-	while (current != NULL)
-	{
-		next = current->next; 
-		current->next = prev; 
-		prev = current;
-		current = next;
-	}
-	*head_ref = prev;
+// Function to reverse the linked list
+void reverse(struct Node** head_ref) {
+    struct Node* prev = NULL;
+    struct Node* current = *head_ref;
+    struct Node* next = NULL;
+    
+    while (current != NULL) {
+        next = current->next;   // Store next node
+        current->next = prev;   // Reverse the current node's pointer
+        prev = current;         // Move pointers one position ahead
+        current = next;
+    }
+    *head_ref = prev;
 }
 
-/* Function to push a node */
-void push(struct Node** head_ref, int new_data)
-{
-	/* allocate node */
-	struct Node* new_node =
-			(struct Node*) malloc(sizeof(struct Node));
-			
-	/* put in the data */
-	new_node->data = new_data;
-				
-	/* link the old list of the new node */
-	new_node->next = (*head_ref); 
-		
-	/* move the head to point to the new node */
-	(*head_ref) = new_node;
+// Function to print linked list
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
 }
 
-/* Function to print linked list */
-void printList(struct Node *head)
-{
-	struct Node *temp = head;
-	while(temp != NULL)
-	{
-		printf("%d ", temp->data); 
-		temp = temp->next; 
-	}
-} 
+// Function to push a node at the beginning
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
 
-/* Driver program to test above function*/
-int main()
-{
-	/* Start with the empty list */
-	struct Node* head = NULL;
+int main() {
+    struct Node* head = NULL;
 
-	push(&head, 20);
-	push(&head, 4);
-	push(&head, 15); 
-	push(&head, 85);	 
-	
-	printf("Given linked list\n");
-	printList(head); 
-	reverse(&head);					 
-	printf("\nReversed Linked list \n");
-	printList(head); 
-	getchar();
+    push(&head, 20);
+    push(&head, 4);
+    push(&head, 15);
+    push(&head, 85);
+
+    printf("Given Linked List\n");
+    printList(head);
+
+    reverse(&head);
+
+    printf("Reversed Linked List\n");
+    printList(head);
+
+    return 0;
 }
