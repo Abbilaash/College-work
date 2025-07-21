16599dbda442ff82c18144a558d773d5ded85a2d
A T Abbilaash <78356376+Abbilaash@users.noreply.github.com>
2025-07-21 11:03:07 +0530

    Create KNN-CNN.py

---
+++ SEM5/Machine Learning 2/KNN-CNN.py
@@ -0,0 +1,30 @@
+coord = [(1,1),(2,1),(4,3),(5,4),(3,2)]
+classes = ['Red','Red','Blue','Blue','Red']
+point = ['A','B','C','D','E']
+z = []
+left_out = [i for i in range(len(coord))]
+
+def distance(a,b):
+    return ((b[0]-a[0])**2+(b[1]-a[1])**2)**0.5
+    
+while left_out:
+    i = left_out.pop(0)
+    if len(z)==0:
+        z.append(i)
+    else:
+        min_len = 10**8
+        min_ind = -1
+        for j in range(len(z)):
+            if distance(coord[i],coord[j])<min_len:
+                min_len = distance(coord[i],coord[j])
+                min_ind = j
+        if classes[min_ind]!=classes[i]:
+            z.append(i)
+
+print("Z = {",end="")
+for i in z:
+    print(point[i],end=" ")
+print("}")
+
+# OUTPUT:
+# Z = {A C D }
