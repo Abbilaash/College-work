f409abca94e20f1e04f222e24a0320e74cfd3442
A T Abbilaash <78356376+Abbilaash@users.noreply.github.com>
2026-02-03 15:43:49 +0530

    Create 10.py

---
+++ SEM6/Deep Learning Lab/Practice Questions/10.py
@@ -0,0 +1,31 @@
+'''
+Implement 2-2-1 MLP for XOR gate
+Use Sigmoid as activation function
+Loss: MSE
+only one forward pass
+'''
+
+import math
+
+X = (1,1)
+y = [0]
+learning_rate = 0.1
+v1,v2=0.3,0.4
+w11,w12,w21,w22,b,w31,w32=0.5,0.4,0.5,0.6,0.1,0.2,0.1
+
+def activation(x):
+    return 1/(1+math.exp(-x))
+
+def forward(X,y,learning_rate):
+    x1,x2 = X
+    y = y[0]
+    h1 = activation(w11*x1+w21*x2+w31)
+    h2 = activation(w12*x1+w22*x2+w32)
+    y_pred = activation(v1*h1+v2*h2+b)
+
+    mse = (y-y_pred)**2
+    print(f"Mean Squared Error: {mse}")
+
+forward(X,y,learning_rate)
+    
+
